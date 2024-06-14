eec8b19a3ac4fa95af7c168b14778b55229608e1
AbdulSaboor-git <abdulsaboor11524@gmail.com>
2024-06-14 22:08:24 +0500

    ui Improvements

--- AnalysisForm.cs
+++ AnalysisForm.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -18,7 +12,6 @@ namespace Gym_Manager
     {
         string connectionString = ConfigurationManager.ConnectionStrings["GymManagementSystemDb"].ConnectionString;
 
-
         public AnalysisForm()
         {
             InitializeComponent();
@@ -26,25 +19,31 @@ namespace Gym_Manager
             comboBox1.SelectedIndex = 0;
         }
 
-         private void AnalysisForm_Load(object sender, EventArgs e)
+        private void AnalysisForm_Load(object sender, EventArgs e)
         {
             Chart_creation();
         }
+
         private void Chart_creation()
         {
-            string query="";
+            string query = "";
             DataTable dataTable = new DataTable();
             switch (comboBox1.SelectedIndex)
             {
                 case 0:
-                 query = "select count(m.MemberID) as Members, e.TypeName from Members m\r\ninner join MembershipTypes e on m.MembershipTypeID=e.MembershipTypeID\r\ngroup by e.TypeName";
-                 dataTable = ExecuteQuery(query);
+                    query = "SELECT e.TypeName AS 'Membership Type', COUNT(m.MemberID) AS 'No. of Members' " +
+                            "FROM Members m " +
+                            "INNER JOIN MembershipTypes e ON m.MembershipTypeID = e.MembershipTypeID " +
+                            "GROUP BY e.TypeName";
+                    dataTable = ExecuteQuery(query);
                     break;
                 case 1:
-                query = "select count(m.MemberID) as Total_Members,s.Position from Staff s\r\ninner join members m on StaffID=TrainerID\r\ngroup by s.Position";
-                dataTable = ExecuteQuery(query);
+                    query = "SELECT s.staff_Name AS 'Trainer Name', s.Position AS 'Role', COUNT(m.MemberID) AS 'No. of Students' " +
+                            "FROM Staff s " +
+                            "INNER JOIN Members m ON s.StaffID = m.TrainerID " +
+                            "GROUP BY s.staff_Name, s.Position";
+                    dataTable = ExecuteQuery(query);
                     break;
-
             }
 
             if (dataTable != null)
@@ -53,19 +52,20 @@ namespace Gym_Manager
                 PopulateDataGridView(dataTable);
             }
         }
+
         private void PopulateDataGridView(DataTable dataTable)
         {
             chartdatagrid.DataSource = dataTable;
         }
+
         private void PopulateChart(DataTable dataTable)
         {
+            MembershipChart.Series.Clear();
+            MembershipChart.Titles.Clear();
+
             if (comboBox1.SelectedIndex == 0)
             {
-
-
-                MembershipChart.Series.Clear();
-                MembershipChart.Titles.Clear();
-                MembershipChart.Titles.Add("Membership Types Distribution");
+                MembershipChart.Titles.Add("Members Distribution based on Membership Type");
 
                 Series series = new Series
                 {
@@ -78,35 +78,67 @@ namespace Gym_Manager
 
                 foreach (DataRow row in dataTable.Rows)
                 {
-                    series.Points.AddXY(row["TypeName"], row["Members"]);
+                    series.Points.AddXY(row["Membership Type"], row["No. of Members"]);
                 }
+                series["PieLabelStyle"] = "Outside";
+                series.BorderWidth = 1;
+                series.BorderColor = Color.Black;
+                series["PieLineColor"] = "Black";
+                series.LegendText = "#VALX (#PERCENT{P0})";
 
-                MembershipChart.Invalidate();
+                MembershipChart.Legends[0].Docking = Docking.Bottom;
+                MembershipChart.Legends[0].Alignment = StringAlignment.Center;
             }
-            else if(comboBox1.SelectedIndex==1)
+            else if (comboBox1.SelectedIndex == 1)
             {
-
-                MembershipChart.Series.Clear();
-                MembershipChart.Titles.Clear();
-                MembershipChart.Titles.Add("Training Types Distribution");
+                MembershipChart.Titles.Add("Members Distribution based on Trainer's Role");
 
                 Series series = new Series
                 {
-                    Name = "Training Type",
+                    Name = "Trainer Type",
                     IsVisibleInLegend = true,
-                    ChartType = SeriesChartType.Bar
+                    ChartType = SeriesChartType.Bar,
+                    Color = Color.FromArgb(139, 95, 44),
+                    BorderWidth = 1,
+                    BorderColor = Color.Black
                 };
 
                 MembershipChart.Series.Add(series);
 
                 foreach (DataRow row in dataTable.Rows)
                 {
-                    series.Points.AddXY(row["Position"], row["Total_members"]);
+                    series.Points.AddXY(row["Role"], row["No. of Students"]);
                 }
 
-                MembershipChart.Invalidate();
+                series.IsValueShownAsLabel = true;
+                series.LabelForeColor = Color.Black;
+                series.LabelFormat = "0"; 
+
+                MembershipChart.ChartAreas[0].AxisX.Title = "Role";
+                MembershipChart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+                MembershipChart.ChartAreas[0].AxisX.Interval = 1;
+                MembershipChart.ChartAreas[0].AxisX.LabelStyle.Font = new Font("Arial", 8);
+
+                MembershipChart.ChartAreas[0].AxisY.Title = "No. of Students";
+                MembershipChart.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+                MembershipChart.ChartAreas[0].AxisY.LabelStyle.Font = new Font("Arial", 8);
+
+                foreach (DataPoint point in series.Points)
+                {
+                    point.Color = Color.FromArgb(139, 95, 44);
+                    point.BackSecondaryColor = Color.FromArgb(205, 175, 149);
+                    point.BackGradientStyle = GradientStyle.DiagonalRight;
+                }
+
+                MembershipChart.ChartAreas[0].AxisY.MinorGrid.Enabled = true;
+                MembershipChart.ChartAreas[0].AxisY.MinorGrid.LineDashStyle = ChartDashStyle.Dot;
+                MembershipChart.ChartAreas[0].AxisY.MinorGrid.LineColor = Color.LightGray;
             }
+
+            MembershipChart.Invalidate();
         }
+
+
         private DataTable ExecuteQuery(string query)
         {
             try
@@ -137,5 +169,4 @@ namespace Gym_Manager
             Chart_creation();
         }
     }
-
-    }
+}
